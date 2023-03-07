36694bdbc915014042fb15f75db959367074a84c
Anas-Alfaihan <narotoaozomake99@gmail.com>
2023-03-07 17:26:17 +0200

    working on the edit login in the page

--- static/js/autocomplete.js
+++ static/js/autocomplete.js
@@ -89,8 +89,7 @@ function autocomplete(inp, arr) {
         }
     }
     function closeAllLists(elmnt) {
-        /*close all autocomplete lists in the document,
-      except the one passed as an argument:*/
+        
         var x = document.getElementsByClassName('autocomplete-items');
         for (var i = 0; i < x.length; i++) {
             if (elmnt != x[i] && elmnt != inp) {
